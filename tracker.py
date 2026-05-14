9cedf0496581eb3db48cd54a0e3fdf7e5c2b32bb
Aye Thandar Phyo <NiroshaSubramani18@gmail.com>
2026-05-14 12:24:24 +0100

    feat: add initial exercise and weight input logic

---
+++ tracker.py
@@ -0,0 +1,14 @@
+# --- Workout Tracker: Sprint 1 ---
+
+# 1. Capture User Input (Strings)
+exercise = input("What exercise did you do?: ")
+weight_txt = input("How much weight did you lift (kg)?: ")
+
+# 2. Type Conversion (Fundamentals)
+weight = float(weight_txt)
+
+# 3. Output the result using an f-string
+print(f"Success! You logged {exercise} at {weight}kg.")
+
+
+
